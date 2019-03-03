df719e5d1f3c08604444648455900bc2cd381d08
King Pra <kingi.pra@gmail.com>
2019-03-03 18:08:13 -0500

    css applied

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 import data from "./reviews.js";
 
-//prints stars depending on rating
+//html selectors
+const container = document.querySelector(".js-review__container");
+const button = document.querySelector(".review__button");
+
+button.addEventListener("click", onClick);
+
+// Toggle Google Reviews on/off
+function onClick() {
+  const show = "Show Reviews";
+  const hide = "Hide Reviews";
+  if (button.innerHTML === show) {
+    button.innerHTML = hide;
+    container.classList.remove("hide");
+    container.classList.add("js-review__container--show");
+  } else {
+    button.innerHTML = show;
+    container.classList.add("hide");
+    container.classList.remove("js-review__container--show");
+  }
+  getData();
+}
+
+//display stars depending on rating returned from data
 function stars(num) {
   let stars = [];
   for (let i = 0; i < 5; i++) {
@@ -12,15 +34,23 @@ function stars(num) {
 }
 
 // dynamically generates list of reviews returned from data
-const container = document.querySelector(".container");
-container.innerHTML = data
-  .map(
-    review => `
-    <ul class="frame">
-        <li class="name">${review.name}</li>
-        <li class="rating">${stars(review.rating)}</li>
-        <li class="comments">${review.review}</li>
-    </ul>
-    `
-  )
-  .join(" ");
+function getData() {
+  container.innerHTML = data
+    .map(
+      review => `
+        <ul class="review__card">
+            <div class="name-container">
+                <div class="icon-container">
+                    <li class="icon">${review.name[0]}</li>
+                </div>
+                <li class="name">${
+                  review.name
+                }<span class="review-source">-Google Reviews</span></li>
+            </div>
+            <li class="rating">${stars(review.rating)}</li>
+            <li class="comments">${review.review}</li>
+        </ul>
+        `
+    )
+    .join(" ");
+}
